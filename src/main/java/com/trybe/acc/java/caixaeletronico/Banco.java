85b682c0b827f742376c6eccdb7a028a6a4d26ef
Ana Luisa <ana.luisa.isa@hotmail.com>
2023-02-24 20:18:18 -0300

    Implementacao: Realiza primeiro teste de gerar numero de uma nova conta na classe Banco

--- src/main/java/com/trybe/acc/java/caixaeletronico/Banco.java
+++ src/main/java/com/trybe/acc/java/caixaeletronico/Banco.java
@@ -1,5 +1,31 @@
 package com.trybe.acc.java.caixaeletronico;
 
-public class Banco {
+import java.util.ArrayList;
+import java.util.Random;
 
+/**Classe Banco. */
+public class Banco {
+  private ArrayList<PessoaCliente> pessoasClientes = new ArrayList<PessoaCliente>();
+  private ArrayList<Conta> contas = new ArrayList<Conta>();
+  public static String novaConta = "";
+  
+  /**Método de gerar numero, de uma nova conta, de 10 digitos. */
+  public String gerarNumeroNovaConta() {
+    gerarNumerosAleatorios();
+    for (Conta conta : contas) {
+      if (novaConta == "  ") {
+        gerarNumerosAleatorios();
+        break;
+      } 
+    }
+    return novaConta;
+  }
+  
+  private static void gerarNumerosAleatorios() {
+    Random random = new Random();
+    for (int i = 0; i < 10; i += 1) {
+      int numeroAleatorio = random.nextInt(10);
+      novaConta += Integer.toString(numeroAleatorio);
+    }
+  }
 }
--- src/test/java/com/trybe/acc/java/caixaeletronico/BancoTest.java
+++ src/test/java/com/trybe/acc/java/caixaeletronico/BancoTest.java
@@ -1,5 +1,6 @@
 package com.trybe.acc.java.caixaeletronico;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,7 +11,8 @@ class BancoTest {
   @Test
   @DisplayName("1 - Testa o gerador de número único para nova conta.")
   void gerarNumeroNovaContaTest() {
-    fail("Não implementado");
+    Banco banco = new Banco();
+    assertTrue(banco.gerarNumeroNovaConta().length() == 10);
   }
 
   @Test
